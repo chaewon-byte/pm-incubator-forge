af4f406a770408ae5d673337d66d2d026608ecc5
powel858 <shinbeom858@gmail.com>
2025-12-30 13:41:05 +0900

    feat: [박신범] Math 기능 및 switch 키워드 활용 [PMIF-x]

--- SHINBEOM PARK/javascript/javascript-app/src/App.tsx
+++ SHINBEOM PARK/javascript/javascript-app/src/App.tsx
@@ -3,6 +3,8 @@ import { VariableAndDataTypes } from './data_type/VariableAndDataType'
 import { CalculationExample } from './math_operation/CalculationExample'
 import { LogicalOperationExample } from './logical_operation/LogicalOperationExample'
 import { ControlFlowIf } from './control_flow/ControlFlowIf'
+import { FirstProblem } from './problem/FirstProblem'
+import { ControlFlowSwitch } from './control_flow/ControlFlowSwitch'
 
 
 
@@ -15,7 +17,8 @@ function App() {
         <VariableAndDataTypes/>
         <LogicalOperationExample/>
         <ControlFlowIf/>
-        
+        <FirstProblem/>
+        <ControlFlowSwitch/>
       </div>
     </>
   )
