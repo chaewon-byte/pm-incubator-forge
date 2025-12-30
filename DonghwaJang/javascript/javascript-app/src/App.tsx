442fc6d941e428c0f390d848a5caacf3b350593e
dongsu2039-tech <dongsu2039@gmail.com>
2025-12-30 15:28:46 +0900

    feat: [장동화] for 문 활용하여 배열값 합산하기 [PMIF-x]

--- DonghwaJang/javascript/javascript-app/src/App.tsx
+++ DonghwaJang/javascript/javascript-app/src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css'
 import { ControlFlowIf } from './control_flow/ControlFlowIf'
+import { ControlFlowFor } from './control_flow/ControlFlowIFor'
+import { ControlFlowForSummation } from './control_flow/ControlFlowIForsummation'
 import { ControlFlowSwitch } from './control_flow/ControlFlowIswitch'
 import { VariableAndDataTypes } from './data_type/VariableAndDataType'
 import { LogicalOperationExample } from './logical_operation/LogicalOperationExample'
@@ -18,7 +20,9 @@ function App() {
         <ControlFlowIf/>
         <FirstProblem/>
         <ControlFlowSwitch/>
-      
+        <ControlFlowFor/>
+        <ControlFlowForSummation/>
+
       </div>
     </>
   )
